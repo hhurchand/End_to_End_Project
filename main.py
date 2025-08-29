c1a32e39700bb55c294117e001f9f41f4e150196
Haroon Patel <heerenpatel01@gmail.com>
2025-08-28 21:55:44 -0400

    added method to read CSV

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from src.utils.input import CSVLoader   # ✅ fixed import
 import mlflow
 
 with mlflow.start_run():
     n_estimators = 100
-    mlflow.log_param("n_estimators",n_estimators)
+    mlflow.log_param("n_estimators", n_estimators)
+
+    df = CSVLoader().load_file(
+        "C:/Users/Owner/OneDrive/Desktop/GitProject/End_to_End_Project/data/raw/airlines_flights_data.csv"
+    )
+    print("DataFrame Shape:", df.shape)
+    print("First 5 rows:\n", df.head())
