187e529b127b4c60bb3738d8e1caf8dfb0142f0f
wetalo <danielswieca@gmail.com>
2025-09-02 20:43:12 -0400

    Changed variables in main with references to yaml file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import mlflow
 from src.utils.input import csvLoader, yamlLoader
 
+config = yamlLoader().load_file("params.yaml")
+
+n_estimators = config["model"]["n_estimators"]
+csv_path = config["data"]["raw_data"]
+
 with mlflow.start_run():
-    n_estimators = 100
     mlflow.log_param("n_estimators",n_estimators)
 
 csvLoader = csvLoader()
-df = csvLoader.load_file("data/raw/airlines_flights_data.csv")
-print(df)
+df = csvLoader.load_file(csv_path)
 
-config = yamlLoader().load_file("params.yaml")
-print(config)
