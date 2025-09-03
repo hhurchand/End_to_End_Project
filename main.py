fc1408d5e9f88baa3c3835c2dd2efd7d428c7c30
Pamelathanasoulias <pamela.athanasoulias@outlook.com>
2025-09-02 20:46:51 -0400

    MAIN UPDATED

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from src.utils.input import CSVLoader
+from src.utils.input import CSVLoader, YAMLLoader
 import mlflow
 
-with mlflow.start_run():
-    n_estimators = 100
-    mlflow.log_param("n_estimators",n_estimators)
 
-df = CSVLoader().load_file("data/raw/airlines_flights_data.csv")
+config = YAMLLoader().load_file("params.yaml")
+
+n_estimators = config["model"]["n_estimators"]
+input_filepath = config["data"]["raw_data"]
+
+print("n_estimators:", n_estimators)
+print("path:", input_filepath)
+
+with mlflow.start_run():
+
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("raw_data", input_filepath)
+
+
+df = CSVLoader().load_file(file_path=input_filepath)
 print(df.head())
-print("df",df)
+print("df shape:", df.shape)
+
 
