91cc8a59afbe6661218ea8e40893fd926dcfad36
wetalo <danielswieca@gmail.com>
2025-10-09 20:17:26 -0400

    Streamlit futher update

--- strm.py
+++ strm.py
@@ -4,14 +4,8 @@ from src.streamlit_controller import StreamlitController
 controller = StreamlitController()
 controller.load_model()
 
-
-st.title('Spam Ham Prediction')
-
-user_input = st.text_input("Enter your email text:")
-
-
-if user_input:
-    email_content = user_input
+def process_email():
+    email_content = user_text
     controller.SetEmailContent(emailContent=email_content)
     controller.transform_email_content()
     spam = controller.predict_email()
@@ -20,4 +14,11 @@ if user_input:
         st.write("That text is ham")
     elif(spam ==1):
         st.write("That text is spam")
+
+st.title('Spam Ham Prediction')
+
+user_text = st.text_area("Enter your email text:")
+
+if st.button("Process Email"):
+    process_email()
 
